c470018bd24e75cb7ba522926282385c407b1a71
c3rb3ru5 <c3rb3ru5@aabbcc.hopto.org>
2021-04-25 18:43:58 -0300

    qiling

--- karton/unpacker/unpacker.py
+++ karton/unpacker/unpacker.py
@@ -11,11 +11,11 @@ from .__version__ import __version__
 
 log = logging.getLogger(__name__)
 
-def unpacker_module_worker(sample, module) -> Task:
+def unpacker_module_worker(sample, user_config, module) -> Task:
     spec = importlib.util.spec_from_file_location("module.name", module)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
-    module = module.KartonUnpackerModule(sample=sample)
+    module = module.KartonUnpackerModule(sample=sample, config=user_config)
     if module.enabled is True:
         return module.main()
 
@@ -37,6 +37,9 @@ class Unpacker(Karton):
     def args_parser(cls):
         parser = super().args_parser()
         parser.add_argument("--modules", help="Modules Directory", required=True)
+        parser.add_argument("--rootfs", help="Emulator RootFS", default=None, required=False)
+        parser.add_argument("--timeout", help="Emulator Timeout", default=5000, required=False)
+        parser.add_argument("--debug", help="Debug", action='store_true', default=False, required=False)
         return parser
 
     @classmethod
@@ -44,20 +47,27 @@ class Unpacker(Karton):
         parser = cls.args_parser()
         args = parser.parse_args()
         config = Config(args.config_file)
-        service = Unpacker(config=config, modules=args.modules)
+        user_config = {
+            'modules': args.modules,
+            'rootfs': args.rootfs,
+            'timeout': args.timeout,
+            'debug': args.debug
+        }
+        service = Unpacker(config=config, user_config=user_config)
         service.loop()
 
-    def __init__(self, modules: Optional[str] = None, *args, **kwargs) -> None:
+    def __init__(self, user_config: Optional[dict] = None, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        modules = user_config['modules']
         modules = modules or "modules"
         self.modules = glob.glob(f'{modules}/**/*.py', recursive=True)
+        self.user_config = user_config
 
     def process(self, task: Task) -> None:
         sample = task.get_resource("sample")
         for module in self.modules:
-            task = unpacker_module_worker(sample, module)
-            if task is not None:
-                log.info(f"successfully unpacked {sample.name}, sending task")
+            tasks = unpacker_module_worker(sample, self.user_config, module)
+            for task in tasks:
                 self.send_task(task)
 
 if __name__ == "__main__":
